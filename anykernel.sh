84521ebf06020f8ce89c41fdfb9c800efb6c730d
root <root@LeCmnGend.localdomain>
2022-01-26 10:41:40 +0700

    Edit string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=TestKernel by t.me/IamBigN
+kernel.string=FuanDoKernel by t.me/IamBigN
 do.devicecheck=1
 do.modules=0
 do.systemless=1
