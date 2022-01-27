07187c774ae0c91220867154f9d5691210378faa
LeCmnGend <46700663+LeCmnGend@users.noreply.github.com>
2022-01-27 21:12:40 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=FuanDoKernel by @LeCmnGend
+kernel.string=FuanDo Kernel by @LeCmnGend
 do.devicecheck=1
 do.modules=1
 do.systemless=0
