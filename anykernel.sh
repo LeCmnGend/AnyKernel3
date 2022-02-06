366f493fe7741f0da5cbc038f07e2f6ffe874f6c
lecmngend <namhoang1235@gmail.com>
2022-02-06 19:59:13 +0700

    Adapt for raphael

--- anykernel.sh
+++ anykernel.sh
@@ -4,40 +4,35 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=FuanDo Kernel by @LeCmnGend
+kernel.string=FuAnDo Kernel by @LeCmnGend
 do.devicecheck=1
-do.modules=1
-do.systemless=0
+do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
-device.name3=
-device.name4=
-device.name5=
+device.name1=raphael
+device.name2=raphaelin
 supported.versions=10 - 12
 supported.patchlevels=2021-10-01
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=auto;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-
 ## AnyKernel install
 dump_boot;
-write_boot;
 
+write_boot;
 ## end install
 
