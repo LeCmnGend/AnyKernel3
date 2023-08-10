fbac0c4f573d846483909a0090c5bfed7145a46b
Chris Renshaw <osm0sis@outlook.com>
2023-08-10 13:22:03 -0300

    Example: add commented multi-parition attributes functions
    DO NOT MERGE if you have already updated your zip properly for your device

--- anykernel.sh
+++ anykernel.sh
@@ -25,12 +25,11 @@ supported.versions=11 - 14
 '; } # end properties
 
 ### AnyKernel install
-# begin attributes
+## boot files attributes
 attributes() {
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
-
 
 ## boot shell variables
 block=/dev/block/bootdevice/by-name/boot;
@@ -109,7 +108,13 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 ## end boot install
 
 
-## init_boot shell variables
+## init_boot files attributes
+#init_boot_attributes() {
+#set_perm_recursive 0 0 755 644 $ramdisk/*;
+#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+#} # end attributes
+
+# init_boot shell variables
 #block=init_boot;
 #is_slot_device=1;
 #ramdisk_compression=auto;
@@ -125,7 +130,13 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 ## end init_boot install
 
 
-## vendor_kernel_boot shell variables
+## vendor_boot files attributes
+#vendor_boot_attributes() {
+#set_perm_recursive 0 0 755 644 $ramdisk/*;
+#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+#} # end attributes
+
+# vendor_kernel_boot shell variables
 #block=vendor_kernel_boot;
 #is_slot_device=1;
 #ramdisk_compression=auto;
