240149b60e99099b28902ad00d65c3ccaed14b88
Chris Renshaw <osm0sis@outlook.com>
2023-08-10 13:14:20 -0300

    anykernel: remove attributes call (now handled in core)

--- anykernel.sh
+++ anykernel.sh
@@ -84,7 +84,7 @@ if mountpoint -q /data; then
 fi
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh && attributes;
+. tools/ak3-core.sh;
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
