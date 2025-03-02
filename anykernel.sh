cbef3e8f684b098b8e1b4f071ff9e2714efc3f89
LeCmnGend <namhoang1235@gmail.com>
2025-03-02 22:06:30 +0700

    Update android version

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=FuanDo Kernel by t.me/LeCmnGend
+kernel.string=FuanDo Kernel by t.me/MrBigN
 do.devicecheck=1
 do.modules=1
 do.systemless=0
@@ -14,51 +14,8 @@ device.name1=r5x
 device.name2=RMX1911
 device.name3=RMX1919
 device.name4=realme5
-supported.versions=10-12.1
+supported.versions=10-15.1
 '; } # end properties
-
-## Optimize F2FS extension list (@arter97)
-if mountpoint -q /data; then
-  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
-    hash="$(md5sum $list_path | sed 's/extenstion/extension/g' | cut -d' ' -f1)"
-
-    # Skip update if our list is already active
-    if [[ $hash == "43df40d20dcb96aa7e8af0e3d557d086" ]]; then
-      echo "Extension list up-to-date: $list_path"
-      continue
-    fi
-
-    ui_print "Optimizing F2FS extension list.."
-    echo "Updating extension list: $list_path"
-
-    echo "Clearing extension list"
-
-    hot_count="$(grep -n 'hot file extens' $list_path | cut -d':' -f1)"
-    list_len="$(cat $list_path | wc -l)"
-    cold_count="$((list_len - hot_count))"
-
-    cold_list="$(head -n$((hot_count - 1)) $list_path | grep -v ':')"
-    hot_list="$(tail -n$cold_count $list_path)"
-
-    for ext in $cold_list; do
-      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
-    done
-
-    for ext in $hot_list; do
-      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
-    done
-
-    echo "Writing new extension list"
-
-    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
-      [ ! -z $ext ] && echo "[c]$ext" > $list_path
-    done
-
-    for ext in $(cat $home/f2fs-hot.list); do
-      [ ! -z $ext ] && echo "[h]$ext" > $list_path
-    done
-  done
-fi
 
 ### AnyKernel install
 ## boot files attributes
